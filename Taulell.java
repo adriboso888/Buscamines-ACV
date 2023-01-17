9a87f2e2022d62de3e77cb060aafd4264af1048e
nezuko <claudiafarina@inspladelestany.cat>
2023-01-17 16:12:31 +0100

    Prova taulell

--- Taulell.java
+++ Taulell.java
@@ -1,2 +1,15 @@
 public class Taulell {
+    public static int COLUMNES = 10;
+    public static int FILES = 10;
+    static void taulell() {
+
+        for (int i = 0; i < COLUMNES; i++) {
+            System.out.print("  ");
+            for (int j = 0; j < FILES; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+
 }
