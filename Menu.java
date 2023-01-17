4408e80248c27e83375da65f73da1ab93ceba3b7
vadic1234 <vadicesteba@inspladelestany.cat>
2023-01-17 15:23:11 +0100

    Menu del buscamines

--- Menu.java
+++ Menu.java
@@ -1,2 +1,8 @@
 public class Menu {
+    public static void main(String[] args) {
+        System.out.println("BUSCAMINES");
+        System.out.println("1-Començar joc");
+        System.out.println("2-Escollir taulell");
+        System.out.println("3-Dificultat");
+    }
 }
