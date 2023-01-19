b5c486e18c5f4b9bddcf7b73f14fafcd414c8be3
nezuko <claudiafarina@inspladelestany.cat>
2023-01-19 18:48:08 +0100

    Arreglant el main

--- Taulell.java
+++ Taulell.java
@@ -1,16 +1,19 @@
 public class Taulell {
+/*
+    static int[][] taulell() {
 
-    static void taulell() {
         int [][]taulell = new int[Dificultat.files][Dificultat.columnes];
 
         for (int i = 0; i < Dificultat.files; i++) {
             for (int j = 0; j < Dificultat.columnes; j++) {
-                taulell[i][j] = 0;
+                taulell[i][j] = (int) (Math.random()*2);
                 System.out.print("*  ");
                 System.out.print(taulell[i][j]);
             }
             System.out.println();
         }
-    }
+        return taulell;
+    }*/
 
 }
+
