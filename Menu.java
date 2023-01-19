c4eb044c3980e6ffbe46c81203c312f43c20df81
adriboso888 <you@example.com>
2023-01-19 18:13:28 +0100

    fflfjla

--- Menu.java
+++ Menu.java
@@ -1,11 +1,12 @@
 public class Menu {
     public static void main(String[] args) {
-        int [][]taulell = new int[Dificultat.files][Dificultat.columnes];
+
         System.out.println("BUSCAMINES");
         System.out.println("1-Començar joc");
         System.out.println("2-Escollir taulell");
         System.out.println("3-Dificultat");
-        Dificultat.dificultat();
+
+
 
 
     }
