0e0810299b3f461b9b854e8a367fc34dd78abd98
nezuko <claudiafarina@inspladelestany.cat>
2023-01-20 18:07:26 +0100

    Intentant posar les mines en caselles random

--- Taulell.java
+++ Taulell.java
@@ -5,16 +5,31 @@ public class Taulell {
     static int[][] taulell(int caselles) {
 
         int [][]taulell = new int[caselles][caselles];
+        int num = 0, num1 = 0;
 
+        int mines = (int) (Math.pow(caselles, 2) * 0.25);
+        for (int k = 0; k < mines;k++){
+            num = (int) (Math.random()*caselles);
+            num1 = (int) (Math.random()*caselles);
+
+            System.out.print(num);
+            System.out.print(num1);
+        }
         for (int i = 0; i < caselles; i++) {
             for (int j = 0; j < caselles; j++) {
-                taulell[i][j] = (int) (Math.random()*2);
-                System.out.print("*  ");
+
+                if(i == num && j == num1){
+                    System.out.print("1");
+                }else {
+                    System.out.print("0");
+                }
+                taulell[i][j] = num;
                 System.out.print("■  ");
-                System.out.print(taulell[i][j]);
+                //System.out.print(taulell[i][j]);
             }
             System.out.println();
         }
+
         return taulell;
     }
 }
