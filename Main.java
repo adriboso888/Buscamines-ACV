177008f532d9f4e6e09ee6163629ddd036701d2a
adriboso888 <you@example.com>
2023-01-24 14:51:00 +0100

    projecte

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Main {
+public class
+Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int caselles = 0;
