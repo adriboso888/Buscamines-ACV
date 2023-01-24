2096dea7ae0b9a14b50934072d84ed1b3c27d4a0
nezuko <claudiafarina@inspladelestany.cat>
2023-01-24 15:21:28 +0100

    modificacio taulell

--- Taulell.java
+++ Taulell.java
@@ -5,25 +5,26 @@ public class Taulell {
     static int[][] taulell(int caselles) {
 
         int[][] taulell = new int[caselles][caselles];
-        int mines = (int) (Math.pow(caselles, 2) * 0.2) + 1 ;
+        int mines = (int) (Math.pow(caselles, 2) * 0.2) + 1;
         int x = 0, y = 0, num = 0, contador = 0;
 
 
-        for(int i = 0; i < caselles; i++){
-            for(int j = 0;j < caselles; j++){
+        for (int i = 0; i < caselles; i++) {
+            for (int j = 0; j < caselles; j++) {
                 taulell[i][j] = 0;
             }
         }
 
-        for (int k = 0; k < mines;k++){
-            x = (int) (Math.random()*caselles);
-            y = (int) (Math.random()*caselles);
+            for (int k = 0; k < mines; k++) {
+                x = (int) (Math.random() * caselles);
+                y = (int) (Math.random() * caselles);
 
-            taulell[x][y] = 1;
-        }
+                taulell[x][y] = 1;
+            }
 
-        for(int i = 0; i<caselles;i++){
-            for(int j = 0; j<caselles;j++){
+
+        for (int i = 0; i < caselles; i++) {
+            for (int j = 0; j < caselles; j++) {
                 System.out.print("■  ");
                 System.out.print(taulell[i][j]);
             }
